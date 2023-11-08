e7d72a99ea0d173d2b7660ecb62930457e88af04
cladrire <cyriel.ladriere@ugent.be>
2023-11-08 11:23:45 +0100

    restructure and work on AT model

--- compression/main.py
+++ compression/main.py
@@ -12,16 +12,18 @@ from tempfile import TemporaryDirectory
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, datasets
 from compression.evaluation import print_model_size, evaluate
-from compression.base_model_pretrained import MobileNetV2, loss_func
-from compression.base_model import MobileNetV3, _mobilenet_v3_conf
+from compression.models.PANN_pretrained import MobileNetV2, loss_func
+from compression.models.base_model import MobileNetV3, _mobilenet_v3_conf
+from compression.models.AT_pretrained import MN, _mn_conf
 from compression.preprocessing import convert_dataset, load_pkl, save_images, get_labels
-from compression.quantized_model import QuantizableMobileNetV3
+from compression.models.quantized_model import QuantizableMobileNetV3
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # ------------- Testing Env
 PREPROCESSING = False
 BASE_MODEL = False
-BASE_MODEL_PRETRAINED = True
+MODEL_AT = True
+MODEL_PANN = False
 QUANTIZATION = False
 PRUNING = False
 # ------------- Variables
@@ -31,11 +33,12 @@ training_audio_labels = "data/audio/train_curated.csv"
 test_audio_labels = "data/audio/sample_submission.csv"
 training_data = "data/train_curated"
 val_data = "data/test"
-model_pth = "resources/MobileNetV2.pth"
+model_pann = "resources/MobileNetV2.pth"
+model_at = "resources/mn10_as.pt"
 # ------------- Hyperparameters
 image_size = (128, 64)
 batch_size = 64
-n_epochs = 15
+n_epochs = 50
 n_classes = 80
 classes = {'Bark': 0, 'Motorcycle': 1, 'Writing': 2, 'Female_speech_and_woman_speaking': 3, 'Tap': 4, 'Child_speech_and_kid_speaking': 5, 'Screaming': 6, 'Meow': 7, 'Scissors': 8, 'Fart': 9, 'Car_passing_by': 10, 'Harmonica': 11, 'Sink_(filling_or_washing)': 12, 'Burping_and_eructation': 13, 'Slam': 14, 'Drawer_open_or_close': 15, 'Cricket': 16, 'Hiss': 17, 'Frying_(food)': 18, 'Sneeze': 19, 'Chink_and_clink': 20, 'Fill_(with_liquid)': 21, 'Crowd': 22, 'Marimba_and_xylophone': 23, 'Sigh': 24, 'Accordion': 25, 'Electric_guitar': 26, 'Cupboard_open_or_close': 27, 'Bicycle_bell': 28, 'Waves_and_surf': 29, 'Stream': 30, 'Bus': 31, 'Toilet_flush': 32, 'Trickle_and_dribble': 33, 'Tick-tock': 34, 'Keys_jangling': 35, 'Acoustic_guitar': 36, 'Finger_snapping': 37, 'Cheering': 38, 'Race_car_and_auto_racing': 39, 'Bass_guitar': 40, 'Yell': 41, 'Water_tap_and_faucet': 42, 'Run': 43, 'Traffic_noise_and_roadway_noise': 44, 'Crackle': 45, 'Skateboard': 46, 'Glockenspiel': 47, 'Computer_keyboard': 48, 'Whispering': 49, 'Zipper_(clothing)': 50, 'Microwave_oven': 51, 'Bathtub_(filling_or_washing)': 52, 'Male_speech_and_man_speaking': 53, 'Gong': 54, 'Shatter': 55, 'Strum': 56, 'Bass_drum': 57, 'Dishes_and_pots_and_pans': 58, 'Accelerating_and_revving_and_vroom': 59, 'Male_singing': 60, 'Gurgling': 61, 'Walk_and_footsteps': 62, 'Printer': 63, 'Cutlery_and_silverware': 64, 'Chirp_and_tweet': 65, 'Clapping': 66, 'Hi-hat': 67, 'Raindrop': 68, 'Gasp': 69, 'Buzz': 70, 'Drip': 71, 'Chewing_and_mastication': 72, 'Squeak': 73, 'Female_singing': 74, 'Church_bell': 75, 'Mechanical_fan': 76, 'Purr': 77, 'Applause': 78, 'Knock': 79}
 
@@ -71,10 +74,22 @@ def main():
         exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1) # Decay LR by a factor of 0.1 every 7 epochs
         model.cuda()
         model = train_model(model, dataloaders, optimizer, exp_lr_scheduler, n_epochs)
+    elif(MODEL_AT):
+        inverted_residual_setting, last_channel = _mn_conf()
+        model = MN(inverted_residual_setting=inverted_residual_setting, last_channel=last_channel, num_classes=80).to(device)
+        pretrained_weights = torch.load(model_at)
+        model.load_state_dict(pretrained_weights)
 
-    elif(BASE_MODEL_PRETRAINED):
+        print_model_size(model)
+        return
+
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1) # Decay LR by a factor of 0.1 every 7 epochs
+        model.cuda()
+        model = train_model(model, dataloaders, optimizer, exp_lr_scheduler, n_epochs)
+    elif(MODEL_PANN):
         model = MobileNetV2(44100, 1024, 320, 64, 50, 14000, 527).to(device)
-        pretrained_weights = torch.load(model_pth)["model"] # keys: {iteration: , model: }
+        pretrained_weights = torch.load(model_pann)["model"] # keys: {iteration: , model: }
         model.load_state_dict(pretrained_weights)
 
         print_model_size(model)
@@ -83,10 +98,11 @@ def main():
         for param in model.features.parameters():
             param.requires_grad = False
 
-        # model = base_model(model, train_dataloader, val_dataloader, classes, n_epochs)
-        model.fc_audioset = nn.Linear(1024, n_classes, bias=True)
-
+        # Initialize layers that are not frozen
+        model.fc1 = nn.Linear(in_features=1280, out_features=256, bias=True)    # out_features tested: 1024(pretty bad), 512(ok), 128(okok)
+        model.fc_audioset = nn.Linear(256, n_classes, bias=True)
         
+       
         optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
         exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1) # Decay LR by a factor of 0.1 every 7 epochs
         model.cuda()
@@ -180,13 +196,13 @@ def train_model(model, dataloaders, optimizer, scheduler, num_epochs=25):
                     metric = BinaryAccuracy(threshold=0.5).to(device)
                     bcm = BinaryConfusionMatrix(threshold=0.5).to(device)
                     
-
                     # forward
                     # track history if only in train
                     with torch.set_grad_enabled(phase == 'train'):
                         # Outputs: {"clipwise_output": [batch_size, num_classes], "Embedding": }
-                        outputs = model(inputs)["clipwise_output"] if BASE_MODEL_PRETRAINED else model(inputs) 
-                        
+                        outputs = model(inputs)["clipwise_output"] if MODEL_PANN else model(inputs) 
+                        # if(epoch == 5 or epoch == 10 or epoch == 1):
+                        #     print(torch.max(model.fc1.weight))
 
                         # loss = loss_func(outputs, labels)
 
@@ -213,7 +229,10 @@ def train_model(model, dataloaders, optimizer, scheduler, num_epochs=25):
                 epoch_loss = running_loss / len(dataloaders[phase])
                 epoch_acc = running_corrects.double() / len(dataloaders[phase])
 
-                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} TN: {TN} FP: {FP} FN: {FN} TP: {TP}')
+                precision = TP/(TP+FP)
+                recall = TP/(TP+FN)
+                F1 = (2*precision*recall)/(precision+recall)
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} Precision: {precision:.4f} Recall: {recall:.4f} F1: {F1:.4f} TN: {TN} FP: {FP} FN: {FN} TP: {TP}')
 
                 # deep copy the model
                 if phase == 'val' and epoch_acc > best_acc:
